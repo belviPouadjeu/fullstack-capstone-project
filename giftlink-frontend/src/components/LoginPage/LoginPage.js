a3218f201bc4282ba2190df19099b9b9312706e8
belviPouadjeu <pbelvinard21@gmail.com>
2025-07-27 14:45:27 +0000

    app.js updated

--- giftlink-frontend/src/components/LoginPage/LoginPage.js
+++ giftlink-frontend/src/components/LoginPage/LoginPage.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { urlConfig } from '../../../config';
+import { urlConfig } from '../../config';
 import './LoginPage.css';
 
 function LoginPage() {
